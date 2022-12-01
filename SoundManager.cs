6addc6f022f3748c2e6c05c82f4ed7acba5fd6c1
MohamadAli Saeidi <1saeidima@gmail.com>
2022-12-02 01:28:20 +0330

    new file:   SoundManager.cs new file:   SoundManager.asmdef new file:   AudioMixer.mixer

---
+++ SoundManager.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.Audio;
+using Utilities;
+
+public class SoundManager : SingletonMonoBehaviour<SoundManager>
+{
+	[SerializeField] private AudioMixer _audioMixer;
+
+	/// <param name="value">Varies from 0 (Exclusive) and 1 (Inclusive)</param>
+	public void SetMasterVolume(float value)
+	{
+		if (value > 1 || value <= 0)
+		{
+			Debug.LogError("Volume value must be between 0 (Exclusive) and 1 (Inclusive)");
+			Mathf.Clamp(value, 0.001f, 1);
+		}
+
+		_audioMixer.SetFloat("MasterVolume", Mathf.Log10(value) * 20);
+	}
+
+	/// <param name="value">Varies from 0 (Exclusive) and 1 (Inclusive)</param>
+	public void SetMusicVolume(float value)
+	{
+		if (value > 1 || value <= 0)
+		{
+			Debug.LogError("Volume value must be between 0 (Exclusive) and 1 (Inclusive)");
+			Mathf.Clamp(value, 0.001f, 1);
+		}
+
+		_audioMixer.SetFloat("MusicVolume", Mathf.Log10(value) * 20);
+	}
+}
